69c7fa9a9ab52d8cc3c7b6b1b58bab576499031b
Artur Hebda <arturhebda@gmail.com>
2014-05-20 14:14:54 +0200

    Fix requires

--- lib/mongoid_embed_finder.rb
+++ lib/mongoid_embed_finder.rb
@@ -1,8 +1,4 @@
 require "mongoid_embed_finder/version"
-require "mongoid_embed_finder/projector"
-require "mongoid_embed_finder/single_projector"
-require "mongoid_embed_finder/relation_discovery"
-require "mongoid_embed_finder/hierarchy_builder"
 require "mongoid_embed_finder/runner"
 
 module MongoidEmbedFinder
