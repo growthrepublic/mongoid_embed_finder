36f8518b3e8b3b3d841e98a84545bf5d3291cbb0
Artur Hebda <arturhebda@gmail.com>
2014-05-20 10:46:56 +0200

    Refactor NestedQuery out from Runner. Let projector influence query directly.

--- spec/mongoid_embed_finder/projectors/base_spec.rb
+++ spec/mongoid_embed_finder/projectors/base_spec.rb
@@ -3,10 +3,15 @@ require "mongoid_embed_finder/projectors/base"
 
 describe MongoidEmbedFinder::Projectors::Base do
   describe "#projection" do
+    let(:query) do
+      double(
+        scope_parent: nil,
+        execute: double(select: []),
+        child_criteria: double(selector: {}))
+    end
     let(:relation) { double(key: "children") }
-    let(:criteria) { double(selector: { "name" => "Child Name 0" }) }
 
-    subject { described_class.new(relation, criteria) }
+    subject { described_class.new(query, relation) }
 
     it "relies on methods to be implemented" do
       expect { subject.projection }.to raise_error(NotImplementedError)
--- spec/mongoid_embed_finder/projectors/single_spec.rb
+++ spec/mongoid_embed_finder/projectors/single_spec.rb
@@ -2,12 +2,36 @@ require "spec_helper"
 require "mongoid_embed_finder/projectors/single"
 
 describe MongoidEmbedFinder::Projectors::Single do
+  let(:relation) { double(key: "children") }
+  let(:criteria) { double(selector: { "name" => "Child Name 0" }) }
+  let(:projection_result) { [] }
+  let(:query) do
+    double(
+      scope_parent: nil,
+      execute: double(select: projection_result),
+      child_criteria: criteria)
+  end
+
+  subject { described_class.new(query, relation) }
+
   describe "#projection" do
-    let(:relation) { double(key: "children") }
-    let(:criteria) { double(selector: { "name" => "Child Name 0" }) }
-
-    subject { described_class.new(relation, criteria) }
-
     its(:projection) { should eq(relation.key => { "$elemMatch" => criteria.selector }) }
+  end
+
+  describe "#project" do
+    it "extend parent's criteria" do
+      subject.project
+      expect(query).to have_received(:scope_parent).with(subject.projection)
+    end
+
+    it "projects query" do
+      subject.project
+      expect(query).to have_received(:execute)
+      expect(query.execute).to have_received(:select).with(subject.projection)
+    end
+
+    it "returns projection result" do
+      expect(subject.project).to eq projection_result
+    end
   end
 end
